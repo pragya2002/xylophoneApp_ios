fbef82b11ea79062cde51fc0ec8882b665a5659c
Pragya Awasthi <71930683+pragya2002@users.noreply.github.com>
2021-07-15 11:42:54 +0530

    Add files via upload
    first

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    var player: AVAudioPlayer?
+
+    @IBAction func keypressed(_ sender: UIButton) {
+        Piano(a: sender.currentTitle!)
+    }
+
+    func Piano(a:String)
+    {
+        guard let url = Bundle.main.url(forResource: a, withExtension: "wav") else { return }
+
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                try AVAudioSession.sharedInstance().setActive(true)
+
+                /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+                player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+                /* iOS 10 and earlier require the following line:
+                player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+                guard let player = player else { return }
+
+                player.play()
+
+            } catch let error {
+                print(error.localizedDescription)
+            }
+    }
+
+}
+
+
+
